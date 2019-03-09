b903243ffe889dadf37ad61da74cd7df2251a105
suerflowz <moshesher1998@gmail.com>
2019-03-09 11:29:14 +0200

    utilize components to check integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, {useState} from "react";
+import {Button, ButtonGroup} from "./components/button";
 
-const App = props => (<div> hello world</div>)
+const App = () => {
+	const [active, setActive] = useState("-1");
+	return (
+		<ButtonGroup>
+			<Button active={active === "0"} onClick={() => {setActive("0");}}> Option A</Button>
+			<Button active={active === "1"} onClick={() => {setActive("1");}}> Option B</Button>
+			<Button active={active === "2"} onClick={() => {setActive("2");}}> Option C </Button>
+		</ButtonGroup>
+	);
+};
 
 export default App;
