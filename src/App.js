f7527f7d2c2c1aff68fbc40f7bef43197e09dd95
suerflowz <moshesher1998@gmail.com>
2019-03-09 22:19:30 +0200

    use input component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {useState} from "react";
-import {Button, ButtonGroup} from "./components/button";
+import {Button, ButtonGroup, Input} from "./components";
 
 const App = () => {
 	const [active, setActive] = useState("-1");
 	return (
-		<ButtonGroup>
-			<Button active={active === "0"} onClick={() => {setActive("0");}}> Option A</Button>
-			<Button active={active === "1"} onClick={() => {setActive("1");}}> Option B</Button>
-			<Button active={active === "2"} onClick={() => {setActive("2");}}> Option C </Button>
-		</ButtonGroup>
+		<React.Fragment>
+			<ButtonGroup>
+				{["1", "2", "3", "4", "5"].map((text, index) => (
+					<Button active={active === text} onClick={() => {setActive(text);}} key={index}>{text}</Button>
+				))}
+			</ButtonGroup>
+
+			<Input placeholder="hello"> </Input>
+		</React.Fragment>
 	);
 };
 
