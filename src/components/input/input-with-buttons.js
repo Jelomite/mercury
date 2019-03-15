4ef2956a7e7af22ab2427c30995248b5478e62c2
suerflowz <moshesher1998@gmail.com>
2019-03-15 15:08:33 +0200

    create reusable component

--- src/components/input/input-with-buttons.js
+++ src/components/input/input-with-buttons.js
@@ -1,21 +1,18 @@
-import React, {useContext} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import {Button} from "../button";
 import Input from "./input";
 import style from "./input-with-buttons.css";
-import {Context} from "../../store";
 
 const ButtonInput = props => {
-	const {store} = useContext(Context);
-
 	return (
 		<div className={cx("ui", "btn-input", style)}>
 			<Button
 				onClick={props.onClick.left}>
 				{props.left}
 			</Button>
-			<Input type={props.type} value={store.count}/>
+			<Input type={props.type} value={props.value}/>
 			<Button	onClick={props.onClick.right}>
 				{props.right}
 			</Button>
@@ -30,6 +27,7 @@ ButtonInput.propTypes = {
 	onClick: PropTypes.objectOf(
 		PropTypes.func
 	),
+	value: PropTypes.string,
 };
 
 ButtonInput.defaultProps = {
