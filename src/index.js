27fece2de14e0170c0cfab89b793ae552355678f
suerflowz <moshesher1998@gmail.com>
2019-03-15 09:36:49 +0200

    provide context to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, {useReducer} from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./style.css";
+import {Context, initialState, reducer} from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Index = () => {
+	const [store, dispatch] = useReducer(reducer, initialState);
+	return (
+		<Context.Provider value={{store, dispatch}}>
+			<App />
+		</Context.Provider>
+	);
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
