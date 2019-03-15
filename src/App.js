188cd16dc189758f1c2518d3e08cf8cc9837342a
suerflowz <moshesher1998@gmail.com>
2019-03-15 09:44:04 +0200

    use global state with current components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, {useState} from "react";
+import React, {useContext} from "react";
 import {Button, ButtonGroup, Input, ButtonInput} from "./components";
+import {Context} from "./store";
 
 const App = () => {
-	const [active, setActive] = useState("-1");
+	const {store, dispatch} = useContext(Context);
+
 	return (
 		<React.Fragment>
 			<ButtonGroup>
 				{["1", "2", "3", "4", "5"].map((text, index) => (
-					<Button active={active === text} onClick={() => {setActive(text);}} key={index}>{text}</Button>
+					<Button
+						active={store.active === index}
+						onClick={() => {
+							dispatch({type: "SET_ACTIVE", value: index});
+						}}
+						key={index}>{text}
+					</Button>
 				))}
 			</ButtonGroup>
 
-			<Input placeholder="hello"> </Input>
-			<ButtonInput right="+" left="-"/>
+			<Input
+				placeholder="hello"
+				value={store.text}
+				onChange={e => {
+					dispatch({type: "UPDATE_TEXT", value: e.target.value});
+				}}
+			/>
+			<ButtonInput
+				rightOnClick={() => dispatch({type: "INCREMENT"})}
+				leftOnClick={() => dispatch({type: "DECREMENT"})}
+			/>
 		</React.Fragment>
 	);
 };
