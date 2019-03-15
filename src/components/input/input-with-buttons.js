c81d4802ae3f821735f8e4aa5ade0eca3be0745f
suerflowz <moshesher1998@gmail.com>
2019-03-15 09:43:42 +0200

    update components to support onChange event

--- src/components/input/input-with-buttons.js
+++ src/components/input/input-with-buttons.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, {useContext} from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import {Button} from "../button";
 import Input from "./input";
 import style from "./input-with-buttons.css";
+import {Context} from "../../store";
 
-const ButtonInput = props => (
-	<div className={cx("ui", "btn-input", style)}>
-		<Button>{props.left}</Button>
-		<Input type={props.type}/>
-		<Button>{props.right}</Button>
-	</div>
-);
+const ButtonInput = props => {
+	const {store} = useContext(Context);
+
+	return (
+		<div className={cx("ui", "btn-input", style)}>
+			<Button
+				onClick={props.leftOnClick}>
+				{props.left}
+			</Button>
+			<Input type={props.type} value={store.count}/>
+			<Button	onClick={props.rightOnClick}>
+				{props.right}
+			</Button>
+		</div>
+	);
+};
 
 ButtonInput.propTypes = {
 	right: PropTypes.string,
 	left: PropTypes.string,
 	type: PropTypes.string,
+	rightOnClick: PropTypes.func,
+	leftOnClick: PropTypes.func,
+};
+
+ButtonInput.defaultProps = {
+	right: "+",
+	left: "-",
+	type: "number",
+	rightOnClick: () => null,
+	leftOnClick: () => null,
 };
 
 export default ButtonInput;
--- src/components/input/input.js
+++ src/components/input/input.js
@@ -14,6 +14,7 @@ const Input = props => (
 			} :
 			{}
 		}
+		{...props}
 	/>
 );
 
