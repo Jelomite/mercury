073c5d78879531a1535a7ef1863b348353ac30a9
suerflowz <moshesher1998@gmail.com>
2019-03-22 03:19:12 +0200

    implement double component parsing

--- src/contexts/form.js
+++ src/contexts/form.js
@@ -4,8 +4,15 @@ import blueprint from "../blueprint.json";
 export const [FormContext, provideComponent] = generateContext({
 	initialState: {...blueprint},
 	reducer: (state, action) => {
-		const [index, section] = action.type.split("#");
-		state[section][index].value = action.value;
-		return {...state};
+		if (action.type === "regular") {
+			const [index, section] = action.path.split("#");
+			state[section][index].value = action.value;
+			return {...state};
+		} else if (action.type === "nested") {
+			const [index, section, side, innerIndex] = action.path.split("#");
+			state[section][index].options[side][innerIndex].value = action.value;
+			return {...state};
+		}
+
 	},
 });
--- src/views/form.js
+++ src/views/form.js
@@ -1,7 +1,6 @@
 import React from "react";
 import cx from "classnames";
 import * as Question from "../components";
-import blueprint from "../blueprint.json";
 import {FormContext} from "../contexts/form";
 import style from "./form.css";
 
@@ -11,26 +10,122 @@ const questionGen = (question, section, index, {store, dispatch}) => {
 		return <Question.Enum
 			options={question.options}
 			active={store[section][index].value}
-			onClick={value => dispatch({type: `${index}#${section}`, value})}
+			onClick={value => dispatch({type: "regular", path: `${index}#${section}`, value})}
 		/>;
 	case "boolean":
 		return <Question.Enum
 			active={store[section][index].value}
-			onClick={value => dispatch({type: `${index}#${section}`, value})}
+			onClick={value => dispatch({type: "regular", path: `${index}#${section}`, value})}
 		/>;
 	case "number":
 		return <Question.Number
 			value={store[section][index].value}
 			onClick={{
-				right: () => dispatch({type: `${index}#${section}`, value: store[section][index].value + 1}),
-				left: () => dispatch({type: `${index}#${section}`, value: Math.max(store[section][index].value - 1, 0)}),
+				right: () => dispatch({type: "regular", path: `${index}#${section}`, value: store[section][index].value + 1}),
+				left: () => dispatch({type: "regular", path: `${index}#${section}`, value: Math.max(store[section][index].value - 1, 0)}),
 			}}
 		/>;
 	case "text":
 		return <Question.Input
 			value={store[section][index].value}
-			onChange={e => dispatch({type: `${index}#${section}`, value: e.target.value})}
+			onChange={e => dispatch({type: "regular", path: `${index}#${section}`, value: e.target.value})}
 		/>;
+	case "double":
+		return (
+			<div className="double">
+				<section className="right">
+					{
+						store[section][index].options.right.map((innerQuestion, innerIndex) => {
+							switch (innerQuestion.type) {
+							case "enum":
+								return (
+									<React.Fragment>
+										<h5>{innerQuestion.name}</h5>
+										<Question.Enum
+											options={innerQuestion.options}
+											active={store[section][index].options.right[innerIndex].value}
+											onClick={value => dispatch({
+												type: "nested",
+												path: `${index}#${section}#right#${innerIndex}`,
+												value,
+											})}
+										/>
+									</React.Fragment>
+								);
+							case "number":
+								return (
+									<React.Fragment>
+										<h5>{innerQuestion.name}</h5>
+										<Question.Number
+											options={innerQuestion.options}
+											value={store[section][index].options.right[innerIndex].value}
+											onClick={{
+												right: () => dispatch({
+													type: "nested",
+													path: `${index}#${section}#right#${innerIndex}`,
+													value: store[section][index].options.right[innerIndex].value + 1}),
+												left: () => dispatch({
+													type: "nested",
+													path: `${index}#${section}#right#${innerIndex}`,
+													value: Math.max(store[section][index].options.right[innerIndex].value - 1, 0),
+												}),
+											}}
+										/>
+									</React.Fragment>
+								);
+							}
+						}
+						)
+					}
+				</section>
+				<section className="seperator"/>
+				<section className="left">
+					{
+						store[section][index].options.left.map((innerQuestion, innerIndex) => {
+							switch (innerQuestion.type) {
+							case "enum":
+								return (
+									<React.Fragment>
+										<h5>{innerQuestion.name}</h5>
+										<Question.Enum
+											options={innerQuestion.options}
+											active={store[section][index].options.left[innerIndex].value}
+											onClick={value => dispatch({
+												type: "nested",
+												path: `${index}#${section}#left#${innerIndex}`,
+												value,
+											})}
+										/>
+									</React.Fragment>
+								);
+							case "number":
+								return (
+									<React.Fragment>
+										<h5>{innerQuestion.name}</h5>
+										<Question.Number
+											options={innerQuestion.options}
+											value={store[section][index].options.left[innerIndex].value}
+											onClick={{
+												right: () => dispatch({
+													type: "nested",
+													path: `${index}#${section}#left#${innerIndex}`,
+													value: store[section][index].options.left[innerIndex].value + 1}),
+												left: () => dispatch({
+													type: "nested",
+													path: `${index}#${section}#left#${innerIndex}`,
+													value: Math.max(store[section][index].options.left[innerIndex].value - 1, 0),
+												}),
+											}}
+										/>
+									</React.Fragment>
+								);
+							}
+						}
+						)
+					}
+				</section>
+			</div>
+		);
 	default:
 		return null;
 	}
@@ -40,16 +135,20 @@ const Form = () => {
 	const {store, dispatch} = React.useContext(FormContext);
 	return (
 		<div className={cx("form", style)}>
-			{Object.entries(blueprint).map((section, sectionIndex) => (
-				<section className={cx("section", style)} key={sectionIndex}>
-					<h2>{section[0]}</h2>
-					{section[1].map((question, questionIndex) => (
-						<div className="question" key={questionIndex}>
-							<h3>{question.name}</h3>
-							{questionGen(question, section[0], questionIndex, {store, dispatch})}
-						</div>
-					))}
-				</section>
+			{Object.entries(store).map((section, sectionIndex) => (
+				<React.Fragment key={sectionIndex}>
+					<section className={cx("section", style)}>
+						<h2>{section[0]}</h2>
+						{section[1].map((question, questionIndex) => (
+							<div className="question" key={questionIndex}>
+								<h4>{question.name}</h4>
+								{questionGen(question, section[0], questionIndex, {store, dispatch})}
+							</div>
+						))}
+					</section>
+					<hr className="divider"/>
+				</React.Fragment>
+
 			))}
 		</div>
 	);
