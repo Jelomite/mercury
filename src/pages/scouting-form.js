3a0c1e31e2a5062b5081c151f9a6eed2cab0718f
suerflowz <moshesher1998@gmail.com>
2019-03-31 23:21:03 +0300

    add settings page integration

--- src/pages/scouting-form.js
+++ src/pages/scouting-form.js
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import Form from "../views/form";
 import Table from "../views/team-selector";
+import Settings from "../views/settings";
 import {provideComponent} from "../contexts/form";
 import {provideMatch} from "../contexts/match";
 
 const ScoutingForm = props => (
 	<React.Fragment>
+		<Settings />
 		<Table match={props.match}/>
 		<Form />
 	</React.Fragment>
---
+++ src/views/settings.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import cx from "classnames";
+import style from "./settings.css";
+import gearIcon from "../img/settings.svg";
+import closeIcon from "../img/close.svg";
+
+const Gear = props => (
+	<div className={cx("icon", style)}>
+		<img src={gearIcon} alt="settings" {...props}/>
+	</div>
+);
+
+const Modal = props => (
+	<div id="root" className={cx("modal", style)}>
+		<img className="close" src={closeIcon} alt="close" {...props}/>
+		<div className="content">
+			<h1>Settings</h1>
+		</div>
+	</div>
+);
+
+const Settings = () => {
+	const [visible, setVisible] = useState(false);
+	return visible ? (
+		<Modal onClick={() => setVisible(!visible)}/>
+	) : (
+		<Gear onClick={() => setVisible(!visible)}/>
+	);
+};
+
+export default Settings;
