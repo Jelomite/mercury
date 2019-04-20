64348ee2e188c90b70af55b3f67a0a127e7499c6
suerflowz <moshesher1998@gmail.com>
2019-04-20 23:40:18 +0300

    restructure firebase

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ import SignPage from "./pages/sign-up";
 
 // firebase imports
 import withFirebaseAuth from "react-with-firebase-auth";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "./firebase-config";
+import {providers, firebaseApp} from "./firebase";
 
-//https://medium.com/firebase-developers/how-to-setup-firebase-authentication-with-react-in-5-minutes-maybe-10-bb8bb53e8834
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAppAuth = firebaseApp.auth();
-const providers = {
-	googleProvider: new firebase.auth.GoogleAuthProvider(),
-};
 // if the user isn't signed in, we give him the sign-in page,
 // otherwise we prompt him to the authorized content
 const App = props => {
